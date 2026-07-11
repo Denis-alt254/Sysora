563c92912fc778a76f753315915712b05119aaa2
Denis-alt254 <deniskipkurui2024@gmail.com>
2026-07-11 23:42:44 +0300

    modified the navbar

--- client/src/components/nav-bar.jsx
+++ client/src/components/nav-bar.jsx
@@ -1,16 +1,23 @@
 import { Link } from "react-router-dom"
+import Button from "./button"
 
 function NavBar() {
     return(
         <div className="navbar">
             <div className="logo">
-                <Link className="" to='/home'>Sysora</Link>
+                <Link className="account-tree" to='/home'><img className = 'account-t' src="../account-tree.png" alt="account tree" />Sysora</Link>
             </div>
             <div className="nav-links">
-                <Link to='/home' >Home</Link>
-                <Link to='/architecture' >Architecture</Link>
-                <Link to='/documentation' >Documentation</Link>
-                <Link to='/ai-lab' >Ai Lab</Link>
+                <div className="links">
+                    <Link to='/home' >Home</Link>
+                    <Link to='/architecture' >Architecture</Link>
+                    <Link to='/documentation' >Documentation</Link>
+                    <Link to='/ai-lab' >Ai Lab</Link>
+                </div>
+                <div className="registration">
+                    <Link to='/login'>Login</Link>
+                    <Link to='/register' className="text-white"><Button>Get Started</Button></Link>
+                </div>
             </div>
         </div>
     )
