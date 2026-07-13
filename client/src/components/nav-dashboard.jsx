734f993462d11df90c78f6fb0d9310802bf29f20
Denis-alt254 <deniskipkurui2024@gmail.com>
2026-07-13 22:50:54 +0300

    nav-dashboard component

---
+++ client/src/components/nav-dashboard.jsx
@@ -0,0 +1,26 @@
+import Button from "./button";
+
+export default function NavDashboard(){
+    return(
+        <>
+            <div className="bg-white p-3 nav-dashboard flex flex-row justify-between h-16">
+                <div className="search-container">
+                    <img className="search-icon" src="../search.png" alt="" />
+                    <input type="text" placeholder="Search systems, deployments and logs..."/>
+                </div>
+                <div className="flex flex-row gap-7 w-125 items-center justify-center">
+                    <img className="icons" src="../notification.png" alt="" />
+                    <img className="icons" src="../apps.png" alt="" />
+                    <Button>Deploy</Button>
+                    <div className="flex flex-row gap-4 w-2xl pr-0">
+                        <div className="flex flex-col">
+                            <h3>Alex Chen</h3>
+                            <p>Senior Architect</p>
+                        </div>
+                        <img className="profile" src="../cloudscale.jpg" alt="" />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
