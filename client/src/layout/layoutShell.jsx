94610071e6daa996fcdba777e791561b9c3347e4
Denis-alt254 <deniskipkurui2024@gmail.com>
2026-07-13 22:52:37 +0300

    remove navbar from layout

--- client/src/layout/layoutShell.jsx
+++ client/src/layout/layoutShell.jsx
@@ -1,12 +1,8 @@
 import Footer from "../components/footer"
-import Navbar from "../components/nav-bar"
 
 export default function LayoutShell ({children}){
     return(
         <div className="bg-gray-100">
-            <header>
-                <Navbar />
-            </header>
             <main>{children}</main>
             <footer>
                 <Footer />
